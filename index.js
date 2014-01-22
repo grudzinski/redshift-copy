985e84c527407bed6c05a36fcc8fa8d84cd571e9
Yaniv Kessler <yanivk@gmail.com>
2014-01-22 04:00:18 +0200

    expose

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.CopyS3 = require('./lib/RedshiftCopyS3.js');
 module.exports.RetryPolicy = require('./lib/RetryPolicy.js');
-//module.exports.Monitor = require('')
+module.exports.Monitor = require('./lib/Monitor.js');
+module.exports.FlushOperation = require('./lib/FlushOperation.js');
